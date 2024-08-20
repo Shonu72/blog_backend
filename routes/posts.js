25b8ded2c750002451e73df6004ba218afa3fc78
Shourya Sonu <sonu.shour77@gmail.com>
2024-08-20 15:09:56 +0530

    feature : (protect route with middleware)

--- routes/posts.js
+++ routes/posts.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const postController = require("../controllers/post.controller");
+const checkAuthMiddleWare = require("../middleware/check-auth")
 
 const router = express.Router();
 
-router.post("/", postController.save);
-router.get("/", postController.showAllPosts);
-router.get("/:id", postController.showPost);
-router.patch("/:id", postController.updatePost);
-router.delete("/:id", postController.deletePost);
+router.post("/create",checkAuthMiddleWare.checkAuth, postController.save);
+router.get("/showallpost", postController.showAllPosts);
+router.get("/showpost/:id", postController.showPost);
+router.patch("/updatepost/:id",checkAuthMiddleWare.checkAuth, postController.updatePost);
+router.delete("/deletepost/:id",checkAuthMiddleWare.checkAuth, postController.deletePost);
 
 module.exports = router;
